b7905d76e6581209133736584f62324232052269
Kent Hervey <kent.hervey@gmail.com>
2022-07-02 23:26:39 -0500

    initital commit

---
+++ src/main/java/com/hervey/CouchbaseFromSpringDataCouchBaseDIY070222/CouchbaseFromSpringDataCouchBaseDiy070222Application.java
@@ -0,0 +1,15 @@
+package com.hervey.CouchbaseFromSpringDataCouchBaseDIY070222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class CouchbaseFromSpringDataCouchBaseDiy070222Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(CouchbaseFromSpringDataCouchBaseDiy070222Application.class, args);
+		System.out.println("number of beans:   " + run.getBeanDefinitionNames().length);
+	}
+
+}
---
+++ src/test/java/com/hervey/CouchbaseFromSpringDataCouchBaseDIY070222/CouchbaseFromSpringDataCouchBaseDiy070222ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hervey.CouchbaseFromSpringDataCouchBaseDIY070222;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class CouchbaseFromSpringDataCouchBaseDiy070222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
